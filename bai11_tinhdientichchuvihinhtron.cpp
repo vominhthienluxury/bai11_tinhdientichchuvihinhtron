b425db030fb965d7b2aa71ff9e771b6f7b247dea
vominhthienluxury <thanhsooon1111@gmail.com>
2024-11-18 09:01:17 +0700

    M_PI

--- bai11_tinhdientichchuvihinhtron.cpp
+++ bai11_tinhdientichchuvihinhtron.cpp
@@ -1,19 +1,18 @@
 // bai11_tinhdientichchuvihinhtron.cpp : This file contains the 'main' function. Program execution begins and ends there.
-
+#define _USE_MATH_DEFINES
 #include <iostream>
 using namespace std;
 int main()
 {
-    double bankinh, pi, chuvi, dientich;
+    double bankinh, chuvi, dientich;
     cout << "TINH DIEN TICH CHU VI HINH TRON";
     cout << "\nNhap bankinh:\t";
     cin >> bankinh;
-    cout << "\nTa co pi=3.14 ";
-    pi = 3.14;
-    chuvi = (2 * pi * bankinh);
-    dientich = (pi * (bankinh*bankinh));
+    chuvi = (2 * M_PI * bankinh);
+    dientich = (M_PI * (bankinh*bankinh));
     cout << "\n chu vi: " << chuvi << endl;
     cout << "\n dien tich : " << dientich << endl;
     return 0;
 }
 
+                                                      
